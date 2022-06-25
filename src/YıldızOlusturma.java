14390ea47e73671ead208a899e79adc23c39bf66
Mehmet Ali Börklü <mehmetaliborklu14@gmail.com>
2022-06-26 00:25:57 +0300

    Yıldız oluşturma projesi

--- src/YıldızOlusturma.java
+++ src/YıldızOlusturma.java
@@ -1,6 +1,22 @@
+import java.util.Scanner;
+
 public class YıldızOlusturma {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
+        System.out.print("Sayı Griniz:");
+        int sayi = scanner.nextInt();
+
+        for (int i = 1 ; i <= sayi; i++ ) {
+            for ( int j = 1; j <= (sayi-i); j++) {
+                System.out.print(" ");
+            }
+            for ( int k = 1; k <= (2*i) - 1 ; k ++ ) {
+                System.out.print("*");
+            }
+            System.out.println();
+
+        }
     }
 
 }
